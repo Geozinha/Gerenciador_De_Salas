7c39457763412332d37da771adc64969fa6b46f4
Geozinha <geovanabmatos@gmail.com>
2024-08-08 19:56:55 -0300

    botoes remover e editar

--- GerenciamentoAdmin/firestore.js
+++ GerenciamentoAdmin/firestore.js
@@ -4,4 +4,26 @@ function salvarSala(novaSala) {
   
     return salaRef.add(novaSala.toFirestoreObject());
   }
+
+function removerSala(salaId){
+  const db = firebase.firestore();
+  const salaRef = db.collection("Sala cadastrada").doc(salaId);
+
+  salaRef.delete().then(() =>{
+    document.querySelector(`[data-reserva-id="${salaId}"]`).parentElement.remove();
+  }).catch((error) =>{
+    alert("Erro ao remover a sala", error);
+  })
+}
+
+function editarSala(salaId, novoDados) {
+  const db = firebase.firestore();
+  const salaRef = db.collection('Sala cadastradas').doc(salaId);
+
+  salaRef.update(novoDados).then(() =>{
+    exibirSalaCadastrada({id: salaId, ...novoDados});
+  }).catch((error) =>{
+    alert("Erro ao editar", error)
+  })
+}
   
