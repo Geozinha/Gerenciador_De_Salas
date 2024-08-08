8e01978bb169386ab0e8048c7415c48d92ca76c9
Geovana Matos <geovanabmatos@gmail.com>
2024-08-08 17:20:52 -0300

    Update firestore.js

--- GerenciamentoAdmin/firestore.js
+++ GerenciamentoAdmin/firestore.js
@@ -5,3 +5,4 @@ function salvarSala(novaSala) {
     return salaRef.add(novaSala);
   }
   
+  //Uncaught FirebaseError: Function addDoc() called with invalid data. Data must be an object, but it was: a custom Sala object (found in document Salas Cadastradas/yFtF66ky33il0IkXoNp0)
